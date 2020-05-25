2923fd3e7856bdb416a75432bec8c067395364d6
Charles Graham <charlesgraham@charless-mbp.attlocal.net>
2020-05-24 20:50:55 -0500

    initial commit

---
+++ src/components/PostForm.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createPost } from "../actions/postActions";
+
+class PostForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: "",
+			body: "",
+		};
+
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+		console.log("hello");
+
+		const post = {
+			title: this.state.title,
+			body: this.state.body,
+		};
+
+		// Call action
+		this.props.createPost(post);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Add Post</h1>
+				<form onSubmit={this.onSubmit}>
+					<div>
+						<label htmlFor="">Title: </label>
+						<br />
+						<input
+							type="text"
+							name="title"
+							id=""
+							value={this.state.title}
+							onChange={this.onChange}
+						/>
+						<br />
+						<label htmlFor="">Body: </label>
+						<br />
+						<input
+							type="textarea"
+							name="body"
+							id=""
+							value={this.state.body}
+							onChange={this.onChange}
+						/>
+						<br />
+						<button type="submit">Submit</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+PostForm.propTypes = {
+	createPost: PropTypes.func.isRequired,
+};
+
+export default connect(null, { createPost })(PostForm);
